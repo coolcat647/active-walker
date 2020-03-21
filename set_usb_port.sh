fa058ec1c8b6dfc0673eccdaafd958bb63bae3e2
coolcat647 <b10302017@mail.ntust.edu.tw>
2020-03-22 01:45:36 +0800

    add a* path findiing

--- set_usb_port.sh
+++ set_usb_port.sh
@@ -4,7 +4,8 @@ if [ ! -f "$rule_file" ]; then
     sudo bash -c 'echo "SUBSYSTEMS==\"usb\", ATTRS{idProduct}==\"2303\", ATTRS{manufacturer}==\"Prolific Technology Inc.\", ATTRS{version}==\" 2.00\", MODE=\"0777\", SYMLINK+=\"walker_motor_left\"" >> /etc/udev/rules.d/50-walker_usb_port.rules'
     sudo bash -c 'echo "SUBSYSTEMS==\"usb\", ATTRS{idProduct}==\"2303\", ATTRS{manufacturer}==\"Prolific Technology Inc.\", ATTRS{version}==\" 1.10\", MODE=\"0777\", SYMLINK+=\"walker_motor_right\"" >> /etc/udev/rules.d/50-walker_usb_port.rules'
     sudo bash -c 'echo "SUBSYSTEMS==\"usb\", ATTRS{idProduct}==\"6001\", ATTRS{manufacturer}==\"FTDI\", ATTRS{idVendor}==\"0403\", MODE=\"0666\", SYMLINK+=\"walker_force_sensor\"" >> /etc/udev/rules.d/50-walker_usb_port.rules'
+    sudo bash -c 'echo "SUBSYSTEMS==\"usb\", ATTRS{idProduct}==\"9d0f\", ATTRS{manufacturer}==\"SparkFun\", MODE=\"0777\", SYMLINK+=\"walker_imu_sensor\"" >> /etc/udev/rules.d/50-walker_usb_port.rules'
 fi
 
 sudo udevadm trigger
-#sudo service udev restart
+# sudo service udev restart
