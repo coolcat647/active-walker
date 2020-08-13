193a2c09d7c65e5f39f0449747ce8182504587c0
coolcat647 <b10302017@mail.ntust.edu.tw>
2020-08-14 01:34:07 +0800

    change detection node from py2.7 version to py3.6

--- catkin_ws/src/active_walker/src/scan_image_combine_node.cpp
+++ catkin_ws/src/active_walker/src/scan_image_combine_node.cpp
@@ -34,6 +34,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
 
 // TF
 #include <tf/transform_listener.h>
