aae0141a59e611e1fa95f6b87f6508bb91440a45
coolcat647 <b10302017@mail.ntust.edu.tw>
2020-08-19 01:30:57 +0800

    imu calibration and add to tf_static

--- catkin_ws/src/sensors/razor_imu_9dof/nodes/show_rpy.py
+++ catkin_ws/src/sensors/razor_imu_9dof/nodes/show_rpy.py
@@ -3,8 +3,10 @@
 import rospy
 from sensor_msgs.msg import Imu
 from tf.transformations import euler_from_quaternion
+from geometry_msgs.msg import PoseStamped, Quaternion
 import math
 
+pub_pose = None
 
 def callback(msg):
     q = (
@@ -15,9 +17,21 @@ def callback(msg):
     )
     (roll, pitch, yaw) = euler_from_quaternion(q)
     # print('yaw= {}, pitch={}, roll={}'.format(yaw, pitch, roll))
-    print('yaw= {}, pitch={}, roll={}'.format(yaw * 180.0 / math.pi, pitch * 180.0 / math.pi, roll * 180.0 / math.pi)) 
+    # print('yaw= {}, pitch={}, roll={}'.format(yaw * 180.0 / math.pi, pitch * 180.0 / math.pi, roll * 180.0 / math.pi))
+
+    global pub_pose
+    if pub_pose is not None:
+        pose_msg = PoseStamped()
+        pose_msg.header.frame_id = msg.header.frame_id
+        pose_msg.header.stamp = rospy.Time.now()
+        pose_msg.pose.orientation = msg.orientation
+        pub_pose.publish(pose_msg)
 
 if __name__ == '__main__':
     rospy.init_node('show_rpy_node', anonymous=True)
     rospy.Subscriber("/imu", Imu, callback)
+
+    # global pub_pose
+    pub_pose = rospy.Publisher("imu_vis", PoseStamped, queue_size=1)
+
     rospy.spin()
